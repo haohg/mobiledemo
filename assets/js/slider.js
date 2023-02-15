d4259370450ba361e9b5dbdb0aee2f6c345b7d74
haohuynh <haohuynh.job@gmail.com>
2023-02-16 00:21:27 +0700

    List Service and List Brand Done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,38 +1,41 @@
 window.onload = function () {
-  const sliderItems = document.querySelectorAll('.slider-left__item');
+  // KICH 
+  const sliders = document.querySelectorAll('.slider-left__item');
   const sliderTitles = document.querySelectorAll('.slider-left__title');
-  let bannerIndex = 0;
+  let indexSliderItem = 0;
 
+  // THÊM SỰ KIỆN VÀO TỪNG NÚT BẤM BÊN DƯỚI SLIDER
   sliderTitles.forEach( (sliderTitle, indexCurrent) => {
     sliderTitle.addEventListener('click', function (e) {
       e.preventDefault();
-      removeClassList(sliderTitles, 'slider-left__title--selected');
-      this.classList.add('slider-left__title--selected');
-      moveBanner(indexCurrent);
-      bannerIndex = indexCurrent;
-
+      selectedTitle(sliderTitles, 'slider-left__title--selected', this);
+      swithBanner(indexCurrent, sliders);
+      indexSliderItem = indexCurrent;
     });
   });
 
-  function removeClassList(arr, className) {
+  // THÊM STYLE RIÊNG CHO TỪNG TITLE ĐƯỢC CHỌN
+  function selectedTitle(arr, className, target) {
     arr.forEach(element => {
       element.classList.remove(className);
     });
+    target.classList.add(className);
   }
 
-  function moveBanner(position){
-    sliderItems.forEach( (item) => {
-        item.style.transform = `translateX(-${position*100}%)`;
-    });
-    removeClassList(sliderTitles, 'slider-left__title--selected');
-    sliderTitles[position].classList.add('slider-left__title--selected');
-    bannerIndex++;
-    if(position >= sliderItems.length - 1){
-      bannerIndex = 0;
+  // HÀM DÙNG ĐỂ CHUYỂN ĐỔI CÁC SLIDER
+  function swithBanner(position, sliders){
+    if(position >= sliders.length){
+      position = 0;
     }
+
+    sliders.forEach((slider)=>{
+      slider.style.transform = `translateX(-${position*100}%)`;
+    })
+    selectedTitle(sliderTitles, 'slider-left__title--selected', sliderTitles[position]);
+    indexSliderItem++;
   }
 
   let interval = setInterval(() => {
-    moveBanner(bannerIndex);
+    swithBanner(indexSliderItem, sliders);
   }, 3000)
 };
